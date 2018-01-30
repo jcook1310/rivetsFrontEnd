6d78627957a5edc35976688d7d8ee35b18ce9ec7
sharontheginger <sharontheginger@me.com>
2018-01-29 16:19:30 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar.js'
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+        <Navbar />
+        <br />
+        Rivets displayed with brief summary
+        <br />
+        Rivets displayed with brief summary
+        <br />
+        Rivets displayed with brief summary
+        <br />
+        Rivets displayed with brief summary
+
+      </div>
+    );
+  }
+}
+
+export default App;
