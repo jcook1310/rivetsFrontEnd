3889422653e4d22298749c248e7c283602d1de99
sharontheginger <sharontheginger@me.com>
2018-01-30 16:08:20 -0800

    2nd day

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Router, Route} from 'react-router-dom'
 import './App.css';
 import Navbar from './components/navbar.js'
+import Rivets from './pages/Rivets'
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            rivets: [
+                {
+                   id: 1,
+                   title: 'Gold',
+                   summary: 'Gold rivet',
+                   description: "Gold rivet mined from the hill of San Francisco, forged in fire deep within Mordor."
+                 },
+                 {
+                   id: 2,
+                   title: 'Silver',
+                   summary: 'Silver rivet',
+                   description: "Silver rivet crafted from a silver bullet, plucked out of Dracula's heart."
+                 },
+                 {
+                   id: 3,
+                   title: 'Bronze',
+                   summary: 'Bronze rivet',
+                   description: "Bronze rivet made from the tears and medals of the third place atheletes from the Nagano Olympics."
+                 }
+            ]
+        }
+    }
   render() {
     return (
-        <div>
-        <Navbar />
-        <br />
-        Rivets displayed with brief summary
-        <br />
-        Rivets displayed with brief summary
-        <br />
-        Rivets displayed with brief summary
-        <br />
-        Rivets displayed with brief summary
-
-      </div>
+        <Router>
+            <div>
+                <Navbar />
+                <Route path="/" render={props => (
+                    <small className='subtitle'>Add a rivet</small>
+                )} />
+                <Route path="/rivets" component={Rivets} />
+            </div>
+        </Router>
     );
   }
 }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
+import { mount } from 'enzyme'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+ it('renders create a rivet form', () => {
+     const app = mount(<App />)
+     expect(app.find('.subtitle').text()).toEqual('Add a rivet')
+ })
